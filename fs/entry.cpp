6751d8222e5c6c7d2f139405055f0033730ea0c8
lvivtotoro <7090833+lvivtotoro@users.noreply.github.com>
2018-05-21 18:12:53 +0300

    Many many many fixes. Read the commit description. Added basic Windows support. Added second constructor to entry to support ASCII on Unicode. Added exists for Windows. Added mkdir for Windows. Added mkdirs for Windows. Added is_dir for Windows. Added get_children for Windows. Fixed c_path return type. Added get_stem.

--- fs/entry.cpp
+++ fs/entry.cpp
@@ -5,12 +5,21 @@
     #include<dirent.h>
     #include<unistd.h>
 #else
-    #error Not yet implemented
+    #include<windows.h>
+    #include<wchar.h>
 #endif
 
 #include<cstring>
 
+// Used for entry::entry(const char*).
+static std::wstring convertStrToWstr(std::string s) {
+    return std::wstring(s.begin(), s.end());
+}
+
 namespace fs {
+
+entry::entry(const char* cstr) : path(convertStrToWstr(cstr)) {
+}
 
 entry::entry(fs::str path) : path(path) {
 }
@@ -23,7 +32,8 @@ bool entry::exists() {
         static thread_local struct stat sb;
         return stat(path.c_str(), &sb) == 0;
     #else
-        #error Not yet implemented
+        DWORD w = GetFileAttributesW(path.c_str());
+        return w != INVALID_FILE_ATTRIBUTES;
     #endif
 }
 
@@ -31,7 +41,7 @@ bool entry::mkdir() {
     #if defined(AWFSI_POSIX)
         return ::mkdir(path.c_str(), S_IRWXU | S_IRWXG | S_IRWXO) == 0;
     #else
-        #error Not yet implemented
+        return ::_wmkdir(path.c_str());
     #endif
 }
 
@@ -59,7 +69,23 @@ bool entry::remove() {
             return unlink(path.c_str()) == 0;
         }
     #else
-        #error Not yet implemented
+        // For some god-awful reason, strings passed to
+        // SHFileOperation need to be double null-terminated.
+        fs::str cpy(path);
+        cpy.push_back('\0');
+        struct _SHFILEOPSTRUCTW shfo = {
+            NULL,
+            FO_DELETE,
+            cpy.c_str(),
+            NULL,
+            FOF_SILENT | FOF_NOERRORUI | FOF_NOCONFIRMATION,
+            FALSE,
+            NULL,
+            NULL
+        };
+        int ret = SHFileOperationW(&shfo);
+        cpy.pop_back();
+        return ret == 0;
     #endif
 }
 
@@ -69,7 +95,8 @@ bool entry::is_dir() {
 
         return stat(path.c_str(), &sb) == 0 && S_ISDIR(sb.st_mode);
     #else
-        #error Not yet implemented
+        DWORD fileAttrs = GetFileAttributesW(path.c_str());
+        return (fileAttrs & FILE_ATTRIBUTE_DIRECTORY) != 0;
     #endif
 }
 
@@ -96,7 +123,21 @@ bool entry::get_children(std::vector<fs::entry>& ret) {
         closedir(dp);
         return true;
     #else
-        #error Not yet implemented.
+        HANDLE hFind;
+        WIN32_FIND_DATAW data;
+
+        hFind = FindFirstFileW((path + L"\\*").c_str(), &data);
+        if(hFind != INVALID_HANDLE_VALUE) {
+            do {
+                if(wcscmp(data.cFileName, L".") != 0 && wcscmp(data.cFileName, L"..") != 0) {
+                    ret.push_back(fs::str(path + static_cast<wchar_t>(fs::sep) + data.cFileName));
+                }
+            } while(FindNextFileW(hFind, &data));
+        } else {
+            return false;
+        }
+
+        return true;
     #endif
 }
 
@@ -104,12 +145,17 @@ const fs::str& entry::get_path() const {
     return this->path;
 }
 
-const char* entry::c_path() const {
+const fs::str::value_type* entry::c_path() const {
     return this->path.c_str();
 }
 
 fs::str entry::get_name() const {
     return path.substr(path.find_last_of(fs::sep) + 1);
+}
+
+fs::str entry::get_stem(bool first) const {
+    fs::str name = get_name();
+    return name.substr(0, name.size() - get_ext(first).size() - 1);
 }
 
 fs::str entry::get_ext(bool first) const {
